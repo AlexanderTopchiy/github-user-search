045334629b1179c9c8a8ea943915a978b8ce88f4
Alexander Topchiy <topchiyau@gmail.com>
2019-06-08 14:04:09 +0300

    Added UserData model and mapper to Data layer

---
+++ app/src/main/java/com/wyverx/githubusersearch/data/usersearch/mapper/UserDataMapper.kt
@@ -0,0 +1,19 @@
+package com.wyverx.githubusersearch.data.usersearch.mapper
+
+import com.wyverx.githubusersearch.data.usersearch.model.UserData
+import com.wyverx.githubusersearch.domain.usersearch.model.User
+
+/**
+ * This class map from data models to domain layer models.
+ */
+class UserDataMapper {
+
+    fun mapFromUserDatatoUser(from: UserData) : User {
+        return User(
+            from.login,
+            from.avatarUrl,
+            from.reposUrl,
+            from.name
+        )
+    }
+}
---
+++ app/src/main/java/com/wyverx/githubusersearch/data/usersearch/model/UserData.kt
@@ -0,0 +1,12 @@
+package com.wyverx.githubusersearch.data.usersearch.model
+
+/**
+ * This class represents data layer models.
+ */
+data class UserData (
+
+    val login: String,
+    val avatarUrl: String,
+    val reposUrl: String,
+    val name: String
+)
